578c78c038f7ddabd4886b6823a5fcb35f4504d9
saurabh989759 <saurabh98king@gmail.com>
2024-10-20 12:44:40 +0530

    new

--- src/main/java/AssigmentQuestions/NodeQ/Node.java
+++ src/main/java/AssigmentQuestions/NodeQ/Node.java
@@ -1,11 +1,18 @@
 package AssigmentQuestions.NodeQ;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 public class Node {
      Node left ;
      Node right ;
      Integer data ;
-     Node(Integer data) {
-         this.data = data;
 
+     @NoArgsConstructor
+     @RequiredArgsConstructor
+     Node(){
+          right = null;
+          left = null;
      }
 }
--- src/main/java/AssigmentQuestions/NodeQ/TreeSizeCalculator.java
+++ src/main/java/AssigmentQuestions/NodeQ/TreeSizeCalculator.java
@@ -2,6 +2,7 @@ import AssigmentQuestions.NodeQ.Node;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 public class TreeSizeCalculator implements Callable<Integer> {
     Node node ;
@@ -11,6 +12,14 @@ public class TreeSizeCalculator implements Callable<Integer> {
         this.ex = ex;
     }
     public Integer call() throws Exception {
-        return 1 ;
+        if(node == null)return -1 ;
+        TreeSizeCalculator o1 = new  TreeSizeCalculator(node.left , ex) ;
+        TreeSizeCalculator o2 = TreeSizeCalculator(node.right , ex) ;
+        Future<Integer> f1 = ex.submit(o1) ;
+        Future<Integer> f2 = ex.submit(o2) ;
+
+        int h1 = f1.get() ;
+        int h2 = f2.get() ;
+        return Math.max(h1 , h2)  + 1 ;
     }
 }
