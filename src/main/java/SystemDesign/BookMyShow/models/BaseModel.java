36c7db85233dfaf9eb5740aa8445fd5b8b922be6
saurabh989759 <saurabh98king@gmail.com>
2024-10-24 15:32:40 +0530

    new update

---
+++ src/main/java/SystemDesign/BookMyShow/models/BaseModel.java
@@ -0,0 +1,9 @@
+package SystemDesign.BookMyShow.models;
+
+import java.util.Date;
+
+public class BaseModel {
+    private Long id;
+    private Date createdAt ;
+    private Date lastModifiedAt ;
+}
