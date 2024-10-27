e384094388a18e5c303adc818861223a5c6443a4
saurabh989759 <saurabh98king@gmail.com>
2024-10-27 23:42:36 +0530

    @mappedSuperClass

--- src/main/java/SystemDesign/BookMyShow/models/BaseModel.java
+++ src/main/java/SystemDesign/BookMyShow/models/BaseModel.java
@@ -1,16 +1,21 @@
 package SystemDesign.BookMyShow.models;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 
 public class BaseModel {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date createdAtt;
     private Date lastModifiedAt ;
