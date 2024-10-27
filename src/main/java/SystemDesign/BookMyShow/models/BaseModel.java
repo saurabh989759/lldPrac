92665654fcbc125bb3b4189b595268a2c864197f
saurabh989759 <saurabh98king@gmail.com>
2024-10-27 23:56:04 +0530

    new changes

--- src/main/java/SystemDesign/BookMyShow/models/BaseModel.java
+++ src/main/java/SystemDesign/BookMyShow/models/BaseModel.java
@@ -3,6 +3,8 @@ package SystemDesign.BookMyShow.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
@@ -17,6 +19,11 @@ public class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Date createdAtt;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @LastModifiedDate
     private Date lastModifiedAt ;
 }
