acc0953213df1eb3dc8049a44c181de470bf0d50
saurabh989759 <saurabh98king@gmail.com>
2024-11-07 15:34:14 +0530

    new changes

--- src/main/java/SystemDesign/BookMyShow/models/Show.java
+++ src/main/java/SystemDesign/BookMyShow/models/Show.java
@@ -5,4 +5,8 @@ public class Show extends BaseModel{
     private String title;
     private String author;
     private String publisher;
+    private String description;
+    private Integer price;
+    private String image;
+
 }
